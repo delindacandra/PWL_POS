526d6f07b66d1352f5c1eb2334c3308954985259
Delinda Candrawati Eka Putri <delindap26@gmail.com>
2024-03-02 08:44:55 +0700

    Pertemuan 3 - Praktikum 5 (Implementasi Query Builder)

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -8,7 +8,25 @@ use Illuminate\Support\Facades\DB;
 class KategoriController extends Controller
 {
     public function index(){
-        $data = DB::select('select * from m_kategori');
+        /*$data=[
+            'kategori_kode' => 'SNK',
+            'kategori_nama' => 'Snack/Makanan Ringan',
+            'created_at' => now()
+        ];
+        
+        DB::table('m_kategori')->insert($data);
+        return 'Insert data baru berhasil';*/
+
+        /*$data = DB::select('select * from m_kategori');
+        return view('kategori', ['data' => $data]);*/
+
+        //$row = DB::table('m_kategori')->where('kategori_kode','SNK')->update(['kategori_nama' => 'camilan']);
+        //return 'Update data berhasil. Jumlah data yang diupdate: '.$row.' baris';
+
+        //$row = DB::table('m_kategori')->where('kategori_kode','SNK')->delete();
+        //return 'Delete data berhasil. Jumlah data yang dihapus: '.$row.' baris';
+
+        $data = DB::table('m_kategori')->get();
         return view('kategori', ['data' => $data]);
     }
 }
