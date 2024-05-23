229dd187cd7de50d309e8f02d95e9fddfa054a9c
Delinda Candrawati Eka Putri <delindap26@gmail.com>
2024-05-23 17:20:22 +0700

    modify user model

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -29,5 +30,10 @@ class UserModel extends Authenticatable implements JWTSubject
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+    protected function image(): Attribute{
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/'.$image),
+        );
     }
 }
